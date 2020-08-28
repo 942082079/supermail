f11a1427f4414cd117b6ec2a52be4835d1ffabf1
Mercury <942082079@qq.com>
2020-08-28 11:30:33 +0800

    第二次提交

---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="tab-bar-item" @click="itemClick">
+    <div v-if="!isActive">
+      <slot name="item-icon"></slot>
+    </div>
+    <div v-else>
+      <slot name="item-active-icon"></slot>
+    </div>
+    <div :style="activeStyle">
+      <slot name="item-text"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      path: String,
+      activeColor: {
+        type: String,
+        default: '#d4237a'
+      }
+    },
+    computed: {
+      isActive() {
+        return this.$route.path.indexOf(this.path) !== -1
+      },
+      activeStyle() {
+        return this.isActive? {color: this.activeColor} : {} 
+      }
+    },
+    methods: {
+      itemClick() {
+        this.$route.path == this.path? null: this.$router.push(this.path)
+      }
+    }
+  };
+</script>
+
+<style>
+  .tab-bar-item {
+    flex: 1;
+    text-align: center;
+    height: 49px;
+  }
+
+  .tab-bar-item img {
+    width: 24px;
+    height: 24px;
+  }
+
+</style>
---
+++ src/views/home/childComps/RecommendView.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="recommend">
+    <div v-for="item in recommends" :key="item.index" class="recommend-item">
+      <a :href="item.link">
+        <img :src="item.image" alt="">
+        <div>{{item.title}}</div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    recommends: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  }
+}
+</script>
+
+<style>
+  .recommend {
+    display: flex;
+    text-align: center;
+    padding: 15px 0 25px;
+    border-bottom: 8px solid #eee;
+  }
+
+  .recommend-item {
+    flex: 1;
+  }
+
+  .recommend-item img {
+    width: 93%;
+    margin-bottom: 10px;
+  }
+</style>
